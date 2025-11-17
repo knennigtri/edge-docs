40990719fb4e64fdd66bb60eb32fb91e07b4d49f
Kevin Nennig <knennig213@gmail.com>
2025-11-17 13:07:57 -0500

    Update helloworld.js

--- blocks/helloworld.js/helloworld.js
+++ blocks/helloworld.js/helloworld.js
@@ -1,6 +1,5 @@
 export default function decorate(block) {
-  block.textContent = 'I am Hello World!';
-  //   const h1 = document.createElement('h1');
-  //   h1.textContent = 'I am Hello World!';
-  //   block.appendChild(h1);
+  const h1 = document.createElement('h1');
+  h1.textContent = 'I am Hello World!';
+  block.replaceChildren(h1);
 }
